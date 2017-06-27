7a68a3993f1baa811a13e23cc4a6af5439de1f76
Benoit Laurent <benoit.jacques.felix@gmail.com>
2017-06-27 21:23:04 +0200

    Change Color Design and JS Script

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 $(document).ready(function() {
 
-    $('.category').bind('click', function() {
-        $('.is-showing').removeClass('is-showing');
-        $('.is-active').removeClass('is-active');
-        $(this).addClass('is-showing');
-        $(this).find('h2').addClass('is-active');
+    var $holder = $('section:first');
+    console.log($holder);
+    
+    /* $('section:first').removeClass('category-is-hidden');
+    $('section:first').addClass('category-is-showing');
+    $('section:first').find('nav').addClass('nav-is-active');
+    $('section:first').find('ul').addClass('is-showing');
+    $('section:first').find('.wraper').addClass('is-showing'); */
+    setTimeout(function(){
+        $('.category-is-showing').find('ul').addClass('fade-in');
+        $('.category-is-showing').find('.wraper').addClass('fade-in');
+    }, 1000);
 
+    $('.category-is-hidden').bind('click', function(){
+        var $this = $(this);
+        
+        $('.category-is-showing').addClass('category-is-hidden');
+        $('.category-is-showing').removeClass('category-is-showing');
+        $('.nav-is-active').removeClass('nav-is-active');
+        $('ul.is-showing').removeClass('is-showing fade-in');
+        $('.wraper.is-showing').removeClass('is-showing fade-in');
+        $(this).removeClass('category-is-hidden');
+        $(this).addClass('category-is-showing');
+        $(this).find('nav').addClass('nav-is-active');
+        $(this).find('ul').addClass('is-showing');
+        $(this).find('.wraper').addClass('is-showing');
+        setTimeout(function(){ 
+            $this.find('ul').addClass('fade-in');
+            $this.find('.wraper').addClass('fade-in');
+        }, 1000);
+
+        
     });
 });
